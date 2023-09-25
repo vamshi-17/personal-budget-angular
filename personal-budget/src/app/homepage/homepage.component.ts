0b5a89931ca1de1152ee90069a1b30823cbe8e3e
Vamshi <durganalavamshikrishna@gmail.com>
2023-09-25 19:27:01 -0400

    d3js chart and tasks updated

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -36,14 +36,14 @@ export class HomepageComponent implements AfterViewInit{
       for (var i = 0; i < res.budget.length; i++) {
         this.dataSource.labels.push(res.budget[i].title);
         this.dataSource.datasets[0].data.push(res.budget[i].budget);
-        // this.dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
-        // this.dataSource.labels[i] = res.data.myBudget[i].title;
     }
     this.createChart();
     // this.createD3Chart();
+    this.myD3Chart();
     });
   }
 
+  //function to create pie chart
   createChart() {
     // var ctx = document.getElementById("myChart").getContext("2d");
     var ctx = document.getElementById("myChart") as HTMLCanvasElement;
@@ -53,72 +53,24 @@ export class HomepageComponent implements AfterViewInit{
     });
   }
 
-//   createD3Chart() {
-//     var myDoughnut = new Chart('canvas', {
-//         type: 'doughnut',
-//         data: this.dataSource,
-//         options: {
-//             legend: {
-//                 display: true
-//             },
-//             scales: {
-//                 xAxes: [{
-//                     display: false
-//                   }],
-//                   yAxes: [{
-//                     display: true
-//                   }],
-//             }
-//         }
-//     })
-// }
-
-
-  // createD3Chart() {
-  //   // Set up dimensions and radius for the chart
-  //   const width = 250;
-  //   const height = 250;
-  //   const radius = Math.min(width, height) / 2;
-
-  //   // Create a color scale
-  //   const color = d3.scaleOrdinal()
-  //       .domain(this.dataSource.labels)
-  //       .range(this.dataSource.datasets[0].backgroundColor); // Use the backgroundColor from data
-
-  //   // Create an SVG element within the container
-  //   const svg = d3.select(".donut")
-  //       .append("svg")
-  //       .attr("width", width)
-  //       .attr("height", height)
-  //       .append("g")
-  //       .attr("transform", `translate(${width / 2},${height / 2})`);
-
-  //       let d = 0;
-  //   // Generate a pie layout
-  //   const pie = d3.pie()
-  //       .value(d => d);
-
-  //   // Generate the arc for each slice
-  //   const arc = d3.arc()
-  //       .innerRadius(radius - 45) // Set inner radius for the donut
-  //       .outerRadius(radius);
-
-  //   // Create the donut chart
-  //   const arcs = svg.selectAll("arc")
-  //       .data(pie(this.dataSource.datasets[0].data))
-  //       .enter()
-  //       .append("g")
-  //       .attr("class", "arc");
-
-  //   // Add slices to the chart
-  //   arcs.append("path")
-  //       .attr("d", arc)
-  //       .attr("fill", (d, i) => color(i)); // Use the color scale
-
-  //   // Add labels to the chart
-  //   arcs.append("text")
-  //       .attr("transform", d => `translate(${arc.centroid(d)})`)
-  //       .attr("text-anchor", "middle")
-  //       .text((d, i) => this.dataSource.labels[i]);
-  //   }
+  //function to create doughnut chart
+  myD3Chart() {
+    var chart = document.getElementById("chartId") as HTMLCanvasElement;
+    var chartId = new Chart(chart, {
+      type: "doughnut",
+      data: {
+        labels: this.dataSource.labels,
+        datasets: [
+          {
+            data: this.dataSource.datasets[0].data,
+            backgroundColor: this.dataSource.datasets[0].backgroundColor,
+            hoverOffset: 5,
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+      },
+    });
+  }
 }
